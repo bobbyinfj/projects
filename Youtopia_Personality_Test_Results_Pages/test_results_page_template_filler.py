8b39b8178b9c4b510bbcb74aad5703ba28520533
Bob <robertnakano@gmail.com>
2023-12-05 20:17:38 -0800

    Working Youtopia Test Results Page Filler

--- Youtopia_Personality_Test_Results_Pages/test_results_page_template_filler.py
+++ Youtopia_Personality_Test_Results_Pages/test_results_page_template_filler.py
@@ -1,5 +1,14 @@
-f"""<div class="top-row" style="background-color: {background_color}; ">
-<h2>You are a {tribe} ({mbti})!</h2>
+import pandas as pd
+
+#load df from csv
+csv_filepath = "Youtopia_Personality_Test_Results_Pages/Youtopia Test Results Page Content - Sheet1.csv"
+
+df = pd.read_csv(csv_filepath)
+
+with open("Youtopia_Personality_Test_Results_Pages/test_results_rendered.txt", "w") as file:
+    for index, row in df.iterrows():
+        text=  """<div class="top-row" style="background-color: {background_color}; ">
+<h2>You are {tribe_indefinite_article} {tribe} ({mbti})!</h2>
 [mks_col]
 [mks_one_third]
 <img src="{animal_image_src}">
@@ -26,7 +35,7 @@ f"""<div class="top-row" style="background-color: {background_color}; ">
 <div id="mc_embed_signup_scroll"><!-- ______________________________ -->
 <h5>Stay updated on Youtopia</h5>
 <div class="mc-field-group"><input id="mce-EMAIL" class="required email" name="EMAIL" type="email" value="" placeholder="Email Address"></div>
-<input name="group[22277]" type="hidden" value="32768"><!--ESTJ=1,ISTJ=2,ESFJ=4,ISFJ=8,ESTP=16,ISTP=32,ESFP=64,ISFP=128,ENFJ=256,INFJ=512,ENFP=1024,INFP=2048,ENTJ=4096,INTJ=8192,ENTP=16384,INTP=32768-->
+<input name="group[22277]" type="hidden" value="{tribe_value}"><!--ESTJ=1,ISTJ=2,ESFJ=4,ISFJ=8,ESTP=16,ISTP=32,ESFP=64,ISFP=128,ENFJ=256,INFJ=512,ENFP=1024,INFP=2048,ENTJ=4096,INTJ=8192,ENTP=16384,INTP=32768-->
 <div id="mce-responses" class="clear">
 <div id="mce-error-response" class="response" style="display: none;"></div>
 <div id="mce-success-response" class="response" style="display: none;"></div>
@@ -35,7 +44,7 @@ f"""<div class="top-row" style="background-color: {background_color}; ">
 <div class="clear"><input id="mc-embedded-subscribe" class="bob" name="subscribe" type="submit" value="Save my result and subscribe"></div>
 </div>
 </form></div>
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {{window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->
 [/mks_one_third]
 [mks_two_thirds]
 <h5>Your Deviant Role</h5>
@@ -46,4 +55,16 @@ f"""<div class="top-row" style="background-color: {background_color}; ">
 [/mks_col]
 
 </div>
- 	<link rel="stylesheet" href="/test-results.css">"""
+ 	<link rel="stylesheet" href="/test-results.css">
+    \n""".format(
+        tribe = row['tribe'],
+        mbti = row['mbti'],
+        tribe_indefinite_article = row['tribe_indefinite_article'],
+        background_color = row['background_color'],
+        animal_image_src = row['animal_image_src'],
+        short_description = row['short_description'],
+        deviant_role_image = row['deviant_role_image'],
+        deviant_role_img_src = row['deviant_role_img_src'],
+        tribe_value = row['tribe_value']
+    )
+        file.write(text)
